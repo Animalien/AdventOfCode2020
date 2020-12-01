b36d3740238a1c737980799f4e586f3447c2e93f
Animalien <Animalien@users.noreply.github.com>
2020-12-01 11:37:42 -0800

    Solved the remainder of Day 1 problem
    And refactored part 1 to be nicer in output, and to reuse logic as much as possible between the two parts

--- AdventOfCode2020.cpp
+++ AdventOfCode2020.cpp
@@ -20,38 +20,89 @@ typedef long long BigInt;
 ////////////////////////////
 // Problem 1 - Report Repair
 
-void RunReportRepair()
+void InitReportRepairTestSet(std::set<BigInt>& set)
 {
-	std::set<BigInt> set;
+	printf("Initializing test set:  1721 979 366 299 675 1456\n");
+	set.insert(1721);
+	set.insert(979);
+	set.insert(366);
+	set.insert(299);
+	set.insert(675);
+	set.insert(1456);
+}
 
-	printf("Reading Report Repair input\n");
+void ReadReportRepairInput(const char* fileName, std::set<BigInt>& set)
+{
+	set.clear();
 
-	FILE* pFile = fopen("Day1Input.txt", "rt");
+	FILE* pFile = fopen(fileName, "rt");
 	assert(pFile);
 
+	printf("\nReading input from %s:  ", fileName);
 	char string[16];
 	while ((fgets(string, sizeof(string), pFile) != nullptr) && !feof(pFile))
 	{
-		const BigInt newInt = atoi(string);
-		printf("  Read %lld, ", newInt);
-
-		const BigInt otherInt = 2020 - newInt;
-		printf("other = %lld, ", otherInt);
-
-		if (set.count(otherInt) > 0)
-		{
-			const BigInt prod = newInt * otherInt;
-			printf("other IS in set!  Product = %lld\n", prod);
-			fclose(pFile);
-			return;
-		}
-
-		printf("other is NOT in set\n");
-		set.insert(newInt);
+		const BigInt integer = atoi(string);
+		printf("%lld ", integer);
+		set.insert(integer);
 	}
+	printf("\n");
 
-	printf("Did NOT find valid candidates!!!!\n");
 	fclose(pFile);
+}
+
+void FindReportRepairTwoNums(const std::set<BigInt>& set)
+{
+	for (auto iter = set.cbegin(), iterEnd = set.cend(); iter != iterEnd; ++iter)
+	{
+		const BigInt thisNum = *iter;
+		const BigInt otherNum = 2020 - thisNum;
+		if (set.count(otherNum) > 0)
+		{
+			printf("Found two numbers that sum to 2020!  Product = %lld * %lld = %lld\n", thisNum, otherNum, thisNum * otherNum);
+			break;
+		}
+	}
+}
+
+void FindReportRepairThreeNums(const std::set<BigInt>& set)
+{
+	for (auto iter = set.cbegin(), iterEnd = set.cend(); iter != iterEnd; ++iter)
+	{
+		const BigInt firstNum = *iter;
+
+		auto iter2 = iter;
+		++iter2;
+		for (; iter2 != iterEnd; ++iter2)
+		{
+			const BigInt secondNum = *iter2;
+			const BigInt partialSum = firstNum + secondNum;
+			if (partialSum > 2020)
+				break;
+
+			const BigInt thirdNum = 2020 - partialSum;
+			if (set.count(thirdNum) > 0)
+			{
+				printf("Found three numbers that sum to 2020!  Product = %lld * %lld * %lld = %lld\n", firstNum, secondNum, thirdNum, firstNum * secondNum * thirdNum);
+				return;
+			}
+		}
+	}
+}
+
+void RunReportRepair()
+{
+	printf("Report Repair\n\n");
+
+	std::set<BigInt> set1;
+	InitReportRepairTestSet(set1);
+	FindReportRepairTwoNums(set1);
+	FindReportRepairThreeNums(set1);
+
+	std::set<BigInt> set2;
+	ReadReportRepairInput("Day1Input.txt", set2);
+	FindReportRepairTwoNums(set2);
+	FindReportRepairThreeNums(set2);
 }
 
 
